1a9c037f564ce34635c43115bb25bfdacbbdcb26
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-01-10 15:57:42 +0900

    ログイン画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'posts#new'
+  root to: 'sessions#new'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
   resources :posts do
     collection do
       post 'confirm'
     end
   end
-  resources :users
 end
