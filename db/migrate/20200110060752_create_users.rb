a1d56cde8ed3a4c02482a63493f06d3585023557
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-01-13 16:09:57 +0900

    dbのsyntaxerrorの修正

--- db/migrate/20200110060752_create_users.rb
+++ db/migrate/20200110060752_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name, null: false
-      t.string :email, null: false, unique: true,
-      
+      t.string :email, null: false, unique: true
+
       t.timestamps
     end
   end
