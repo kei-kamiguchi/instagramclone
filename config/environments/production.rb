75dce838d96d38a02dd1c70e412f19be000cfe31
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-01-13 17:13:35 +0900

    herokuのdbリセット

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'vast-lake-28165.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'secure-dawn-24129.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
